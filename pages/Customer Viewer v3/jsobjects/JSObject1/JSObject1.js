16000510fc63a16de35d2e8f83e539ea6ad485d9
Chris Hodgkins <christopher.hodgkins@secureco.co>
2025-11-13 01:45:57 +0000

    One more bug tweak

--- pages/Customer Viewer v3/jsobjects/JSObject1/JSObject1.js
+++ pages/Customer Viewer v3/jsobjects/JSObject1/JSObject1.js
@@ -1,12 +1,9 @@
 export default {
-	myVar1: [],
-	myVar2: {},
-	myFun1 () {
-		//	write code here
-		//	this.myVar1 = [1,2,3]
-	},
-	async myFun2 () {
-		//	use async-await or promises
-		//	await storeValue('varName', 'hello world')
-	}
+  // Function 1: Clear store variables on page initialization
+  onPageLoad: () => {
+    return Promise.all([
+      storeValue('statclickaccount', ''),
+      storeValue('statclick', '')
+    ]);
+  }
 }
