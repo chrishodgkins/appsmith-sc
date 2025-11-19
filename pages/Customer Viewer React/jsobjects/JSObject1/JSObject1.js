80c99620336aa51b64536874fa1d0c586c7909b1
Chris Hodgkins <christopher.hodgkins@secureco.co>
2025-11-19 04:53:28 +0000

    Moved new diagraming tool out

--- pages/Customer Viewer React/jsobjects/JSObject1/JSObject1.js
+++
@@ -1,9 +0,0 @@
-export default {
-  // Function 1: Clear store variables on page initialization
-  onPageLoad: () => {
-    return Promise.all([
-      storeValue('statclickaccount', ''),
-      storeValue('statclick', '')
-    ]);
-  }
-}
